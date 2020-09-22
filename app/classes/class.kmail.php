f428825f97ecdac24af9af8021e961d7be6a4f65
Burak <71403909+burakusoglu@users.noreply.github.com>
2020-09-22 10:34:14 +0300

    Update class.kmail.php

--- app/classes/class.kmail.php
+++ app/classes/class.kmail.php
@@ -18,8 +18,8 @@ class Kmail {
     // $mail->SMTPDebug = 1;
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'destek.bagiskutusu@gmail.com';                     // SMTP username
-    $mail->Password   = 'atilim06.';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '.';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
